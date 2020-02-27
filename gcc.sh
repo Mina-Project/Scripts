9d94211c80542e2cbde48b91a0f55ba048ab051f
Mhmmdfdlyas! <fadlyardhians@gmail.com>
2020-02-27 12:46:45 +0700

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -1,65 +1,32 @@
 #!/usr/bin/env bash
-# Circle CI/CD - kernel build script
-# Support for Xiaomi Redmi 4A & Xiaomi Redmi 5A
-git clone -q -j32 https://github.com/fadlyas07/AnyKernel3-1 --depth=1 zip1
-git clone -q -j32 https://github.com/fadlyas07/AnyKernel3-1 --depth=1 zip2
-git clone -q -j32 https://github.com/fabianonline/telegram.sh --depth=1 telegram
-TELEGRAM_ID=$chat_id
-TELEGRAM_TOKEN=$token
-export TELEGRAM_TOKEN TELEGRAM_ID
+# Circle CI/CD - Simple kernel build script
+export parse_branch=$(git rev-parse --abbrev-ref HEAD)
+     export device="Xiaomi Redmi 4A/5A"
+     export codename_device1=rolex
+     export config_device1=rolex_defconfig
+     export codename_device2=riva
+     export config_device2=riva_defconfig
+mkdir $(pwd)/TEMP
+     mkdir -p $(pwd)/clang
+     wget https://kdrag0n.dev/files/redirector/proton_clang-latest.tar.zst
+     tar -I zstd -xvf *.tar.zst -C $(pwd)/clang --strip-components=1
+git clone --depth=1 https://github.com/fadlyas07/AnyKernel3-1 zip1
+git clone --depth=1 https://github.com/fadlyas07/AnyKernel3-1 zip2
+git clone --depth=1 https://github.com/fabianonline/telegram.sh telegram
+export pack1=$(pwd)/zip1
+export pack2=$(pwd)/zip2
 
-# Device 1
-codename_device1="rolex"
-config_device1="rolex_defconfig"
-
-# Device 2
-codename_device2="riva"
-config_device2="riva_defconfig"
-
-# Github Env Vars
-KERNEL_NAME="GREENFORCE"
-KERNEL_DIR="$(pwd)"
-UNIFIED="Xiaomi Redmi 4A/5A"
-PARSE_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-COMMIT_POINT="$(git log --pretty=format:'<code>%h: %s by</code> <b>%an</b>' -1)"
-TOOLCHAIN_DIR="/root/toolchain"
-pack1="$KERNEL_DIR/zip1"
-pack2="$KERNEL_DIR/zip2"
-KERNEL_IMG="$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb"
-
-# Find kernel branch
-if [ "$PARSE_BRANCH" == "HMP-vdso32" ]; then
-	KERNEL_TYPE=HmP
-	export $KERNEL_TYPE
-	STICKER="CAADBQADeQEAAn1Cwy71MK7Ir5t0PhYE"
-	export $STICKER
-elif [ "$PARSE_BRANCH" == "EAS" ]; then
-	KERNEL_TYPE=EaS
-	export $KERNEL_TYPE
-	STICKER="CAADBQADIwEAAn1Cwy5pf2It72fNXBYE"
-	export $STICKER
-elif [ "$PARSE_BRANCH" == "aosp/android-3.18" ]; then
-	KERNEL_TYPE=Pure-CaF
-	export $KERNEL_TYPE
-	STICKER="CAADBQADfAEAAn1Cwy6aGpFrL8EcbRYE"
-	export $STICKER
-elif [ ! "$KERNEL_TYPE" ]; then
-	KERNEL_TYPE=Test
-	export $KERNEL_TYPE
-	STICKER="CAADBQADPwEAAn1Cwy4LGnCzWtePdRYE"
-	export $STICKER
-fi
-
-# create temp dir for kernel log
-mkdir $KERNEL_DIR/TEMP
-TEMP="$KERNEL_DIR/TEMP"
-
-export STICKER
-export KERNEL_TYPE
+# Telegram & Github Env Vars
 export ARCH=arm64
-export TZ=":Asia/Jakarta"
+export TZ=Asia/Jakarta
+export TEMP=$(pwd)/TEMP
+export TELEGRAM_ID=$chat_id
+export TELEGRAM_TOKEN=$token
+export product_name=GREENFORCE
 export KBUILD_BUILD_USER=github.com.fadlyas07
 export KBUILD_BUILD_HOST=$CIRCLE_SHA1
+export kernel_img=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+export commit_point=$(git log --pretty=format:'<code>%h: %s by</code> <b>%an</b>' -1)
 
 TELEGRAM=telegram/telegram
 tg_channelcast() {
@@ -70,78 +37,71 @@ tg_channelcast() {
 		done
 	)"
 }
-tg_makegcc() {
-	make -j$(nproc) O=out \
-                  CROSS_COMPILE=aarch64-linux-android- \
-                  CROSS_COMPILE_ARM32=arm-linux-androideabi- 2>&1| tee kernel.log
-}
-tg_sendstick() {
-   curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" \
-	-d sticker="$STICKER" \
-	-d chat_id="$TELEGRAM_ID"
-}
-tg_sendinfo() {
-   curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage" \
-	-d "parse_mode=markdown" \
-	-d text="${1}" \
-	-d chat_id="${TELEGRAM_ID}" \
-	-d "disable_web_page_preview=true"
-}
-tg_pushlog() {
-   curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
-	-F chat_id="$fadlyas"
+make_kernel() {
+make -j$(nproc) O=out \
+                ARCH=arm64 \
+                CC=clang \
+                CLANG_TRIPLE=aarch64-linux-gnu- \
+                CROSS_COMPILE=aarch64-linux-gnu- \
+                CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1| tee kernel.log
 }
 tg_makedevice1() {
 make -j$(nproc) O=out ARCH=arm64 $config_device1
-PATH="$TOOLCHAIN_DIR/gcc/bin:$TOOLCHAIN_DIR/gcc32/bin:$PATH" \
-tg_makegcc
+make_kernel
 }
 tg_makedevice2() {
 make -j$(nproc) O=out ARCH=arm64 $config_device2
-PATH="$TOOLCHAIN_DIR/gcc/bin:$TOOLCHAIN_DIR/gcc32/bin:$PATH" \
-tg_makegcc
+make_kernel
 }
-
-# Make device 1
+tg_sendinfo() {
+  curl -s "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
+       -d chat_id="784548477" \
+       -d "parse_mode=markdown" \
+       -d "disable_web_page_preview=true" \
+       -d text="$1"
+}
+tg_sendstick() {
+   curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" \
+	-d sticker="CAADBQADPwEAAn1Cwy4LGnCzWtePdRYE" \
+	-d chat_id="$TELEGRAM_ID"
+}
+export PATH=$(pwd)/clang/bin:$PATH
+export LD_LIBRARY_PATH=$(pwd)/clang/lib:$LD_LIBRARY_PATH
 date1=$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')
 tg_makedevice1
 mv *.log $TEMP
-if [[ ! -f "$KERNEL_IMG" ]]; then
-	tg_pushlog
-	tg_sendinfo "<b>$KERNEL_NAME $KERNEL_TYPE Build Failed</b>!!"
+if [[ ! -f "$kernel_img" ]]; then
+        curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$fadlyas"
+	tg_sendinfo "$product_name $device Build Failed!!"
 	exit 1;
+else
+        mv $kernel_img $pack1/zImage
 fi
-tg_pushlog
-mv $KERNEL_IMG $pack1/zImage
+curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="784548477"
 cd $pack1
-zip -r9q $KERNEL_NAME-$codename_device1-$KERNEL_TYPE-$date1.zip * -x .git README.md LICENCE
+zip -r9q $product_name-$codename_device1-$date1.zip * -x .git README.md LICENCE
 cd ..
-
-# clean out & log for anticipation dirty build
 rm -rf out/ $TEMP/*.log
-
-# Make device 2
 date2=$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')
 tg_makedevice2
 mv *.log $TEMP
-if [[ ! -f "$KERNEL_IMG" ]]; then
-	tg_pushlog
-	tg_sedtemplate
+if [[ ! -f "$kernel_img" ]]; then
+        curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$fadlyas"
+	tg_sendinfo "$product_name $device Build Failed!!"
 	exit 1;
+else
+        mv $kernel_img $pack2/zImage
 fi
-tg_pushlog
-mv $KERNEL_IMG $pack2/zImage
+curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="784548477"
 cd $pack2
-zip -r9q $KERNEL_NAME-$codename_device2-$KERNEL_TYPE-$date2.zip * -x .git README.md LICENCE
+zip -r9q $product_name-$codename_device2-$date2.zip * -x .git README.md LICENCE
 cd ..
-toolchain_ver=$(cat $KERNEL_DIR/out/include/generated/compile.h | grep LINUX_COMPILER | cut -d '"' -f2)
+toolchain_ver=$(cat $(pwd)/out/include/generated/compile.h | grep LINUX_COMPILER | cut -d '"' -f2)
 tg_sendstick
-tg_channelcast 	"<b>$KERNEL_NAME new build is available</b>!" \
-		"<b>Device :</b> <code>$UNIFIED</code>" \
-		"<b>Kernel Type :</b> <code>$KERNEL_TYPE</code>" \
-		"<b>Branch :</b> <code>$PARSE_BRANCH</code>" \
-		"<b>Toolchain :</b> <code>$toolchain_ver</code>" \
-		"<b>Latest commit :</b> $COMMIT_POINT"
+tg_channelcast "<b>$product_name new build is available</b>!" \
+	       "<b>Device :</b> <code>$device</code>" \
+	       "<b>Branch :</b> <code>$parse_branch</code>" \
+               "<b>Toolchain :</b> <code>$toolchain_ver</code>" \
+	       "<b>Latest commit :</b> $commit_point"
 curl -F document=@$(echo $pack1/*.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
 curl -F document=@$(echo $pack2/*.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
-curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" -d sticker="CAACAgQAAxkBAAEUuSReR6voi-DDaqP6FoqEG-pfBdeO9gACnjgAAjGNRgABOsdMK2pVZ7kYBA" -d chat_id="$TELEGRAM_ID"
