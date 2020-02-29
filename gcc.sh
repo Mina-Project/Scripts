cc1f497464ec6094a4b41f5d243ccf3b5d671bf8
Mhmmdfdlyas! <fadlyardhians@gmail.com>
2020-02-29 17:30:53 +0700

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -1,40 +1,38 @@
 #!/usr/bin/env bash
 # Circle CI/CD - kernel build script
+export parse_branch=$(git rev-parse --abbrev-ref HEAD)
+export commit_point=$(git log --pretty=format:'<code>%h: %s by</code> <b>%an</b>' -1)
+
+# Device 1
+export codename_device1=rolex
+export config_device1=rolex_defconfig
+
+# Device 2
+export codename_device2=riva
+export config_device2=riva_defconfig
+
+# Github Env Vars
+export ARCH=arm64
+export TZ="Asia/Jakarta"
+export pack1=$(pwd)/zip1
+export pack2=$(pwd)/zip2
+export kernel_type=EaS
+export TELEGRAM_ID=$chat_id
+export TELEGRAM_TOKEN=$token
+export kernel_name="GREENFORCE"
+export device="Xiaomi Redmi 4A/5A"
+export KBUILD_BUILD_HOST=$CIRCLE_SHA1
+export KBUILD_BUILD_USER=github.com.fadlyas07
+export PATH=$(pwd)/gcc/bin:$(pwd)/gcc32/bin:$PATH
+export kernel_img=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+
+mkdir $(pwd)/TEMP
+export TEMP=$(pwd)/TEMP
 git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r40 gcc
 git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r40 gcc32
 git clone --depth=1 https://github.com/fabianonline/telegram.sh telegram
 git clone --depth=1 https://github.com/fadlyas07/AnyKernel3-1 zip1
 git clone --depth=1 https://github.com/fadlyas07/AnyKernel3-1 zip2
-export TELEGRAM_ID=$chat_id
-export TELEGRAM_TOKEN=$token
-
-# Device 1
-export codename_device1="rolex"
-export config_device1="rolex_defconfig"
-
-# Device 2
-export codename_device2="riva"
-export config_device2="riva_defconfig"
-
-# Github Env Vars
-KERNEL_NAME="GREENFORCE"
-KERNEL_DIR="$(pwd)"
-UNIFIED="Xiaomi Redmi 4A/5A"
-PARSE_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-COMMIT_POINT="$(git log --pretty=format:'<code>%h: %s by</code> <b>%an</b>' -1)"
-pack1="$KERNEL_DIR/zip1"
-pack2="$KERNEL_DIR/zip2"
-KERNEL_IMG="$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb"
-
-# create temp dir for kernel log
-mkdir $KERNEL_DIR/TEMP
-TEMP="$KERNEL_DIR/TEMP"
-
-export ARCH=arm64
-export TZ=":Asia/Jakarta"
-export KERNEL_TYPE=EaS
-export KBUILD_BUILD_USER=github.com.fadlyas07
-export KBUILD_BUILD_HOST=$CIRCLE_SHA1
 
 TELEGRAM=telegram/telegram
 tg_channelcast() {
@@ -64,18 +62,12 @@ tg_sendinfo() {
 		done
 	)"
 }
-tg_pushlog() {
-   curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" \
-	-F chat_id="$fadlyas"
-}
 tg_makedevice1() {
 make O=out ARCH=arm64 $config_device1
-PATH="$KERNEL_DIR/gcc/bin:$KERNEL_DIR/gcc32/bin:$PATH" \
 tg_makegcc
 }
 tg_makedevice2() {
 make O=out ARCH=arm64 $config_device2
-PATH="$KERNEL_DIR/gcc/bin:$KERNEL_DIR/gcc32/bin:$PATH" \
 tg_makegcc
 }
 
@@ -83,15 +75,15 @@ tg_makegcc
 date1=$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')
 tg_makedevice1
 mv *.log $TEMP
-if [[ ! -f "$KERNEL_IMG" ]]; then
-	tg_pushlog
-	tg_sendinfo "<b>$KERNEL_NAME $KERNEL_TYPE Build Failed</b>!!"
-	exit 1;
+if [[ ! -f "$kernel_img" ]]; then
+	curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$fadlyas"
+	tg_sendinfo "<b>$kernel_name $kernel_type Build Failed</b>!!"
+	exit 1
 fi
-tg_pushlog
-mv $KERNEL_IMG $pack1/zImage
+curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$fadlyas"
+mv $kernel_img $pack1/zImage
 cd $pack1
-zip -r9q $KERNEL_NAME-$codename_device1-$KERNEL_TYPE-$date1.zip * -x .git README.md LICENCE
+zip -r9q $kernel_name-$codename_device1-$kernel_type-$date1.zip * -x .git README.md LICENCE
 cd ..
 
 # clean out & log for anticipation dirty build
@@ -101,23 +93,23 @@ rm -rf out/ $TEMP/*.log
 date2=$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')
 tg_makedevice2
 mv *.log $TEMP
-if [[ ! -f "$KERNEL_IMG" ]]; then
-	tg_pushlog
-	tg_sedtemplate
-	exit 1;
+if [[ ! -f "$kernel_img" ]]; then
+	curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$fadlyas"
+	tg_sendinfo "<b>$kernel_name $kernel_type Build Failed</b>!!"
+	exit 1
 fi
-tg_pushlog
-mv $KERNEL_IMG $pack2/zImage
+curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$fadlyas"
+mv $kernel_img $pack2/zImage
 cd $pack2
-zip -r9q $KERNEL_NAME-$codename_device2-$KERNEL_TYPE-$date2.zip * -x .git README.md LICENCE
+zip -r9q $kernel_name-$codename_device2-$KERNEL_TYPE-$date2.zip * -x .git README.md LICENCE
 cd ..
 toolchain_ver=$(cat $KERNEL_DIR/out/include/generated/compile.h | grep LINUX_COMPILER | cut -d '"' -f2)
 tg_sendstick
-tg_channelcast "<b>$KERNEL_NAME new build is available</b>!" \
-		"<b>Device :</b> <code>$UNIFIED</code>" \
-		"<b>Kernel Type :</b> <code>$KERNEL_TYPE</code>" \
-		"<b>Branch :</b> <code>$PARSE_BRANCH</code>" \
+tg_channelcast "<b>$kernel_name new build is available</b>!" \
+		"<b>Device :</b> <code>$device</code>" \
+		"<b>Kernel Type :</b> <code>$kernel_type</code>" \
+		"<b>Branch :</b> <code>$parse_branch</code>" \
 		"<b>Toolchain :</b> <code>$toolchain_ver</code>" \
-		"<b>Latest commit :</b> $COMMIT_POINT"
+		"<b>Latest commit :</b> $commit_point"
 curl -F document=@$(echo $pack1/*.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
 curl -F document=@$(echo $pack2/*.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
