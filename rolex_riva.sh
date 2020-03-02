390b79dbc9ffa947ba0ebdcc4e68e29d7951eaae
Mhmmdfdlyas! <fadlyardhians@gmail.com>
2020-03-02 20:33:20 +0700

    Update rolex_riva.sh

--- rolex_riva.sh
+++ rolex_riva.sh
@@ -58,12 +58,12 @@ tg_channelcast() {
 	)"
 }
 tg_makeclang() {
-	make -j$(nproc) O=out \
-			ARCH=arm64 \
-			CC=clang \
-			CLANG_TRIPLE=aarch64-linux-gnu- \
-			CROSS_COMPILE=aarch64-linux-android- \
-			CROSS_COMPILE_ARM32=arm-linux-androideabi- 2>&1| tee kernel.log
+make -j$(nproc) O=out \
+		ARCH=arm64 \
+		CC=clang \
+		CLANG_TRIPLE=aarch64-linux-gnu- \
+		CROSS_COMPILE=aarch64-linux-android- \
+		CROSS_COMPILE_ARM32=arm-linux-androideabi- 2>&1| tee kernel.log
 }
 tg_sendstick() {
    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker" \
