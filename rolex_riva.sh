09f9730de793743dacc3424f9c5e8fe2b6197df9
Mhmmdfdlyas! <fadlyardhians@gmail.com>
2020-03-21 15:50:28 +0700

    Update rolex_riva.sh

--- rolex_riva.sh
+++ rolex_riva.sh
@@ -7,7 +7,7 @@ export parse_branch=$(git rev-parse --abbrev-ref HEAD)
 if [ "$parse_branch" == "HMP-vdso32" ]; then
 	export kernel_type=Hmp
 	export sticker="CAADBQADeQEAAn1Cwy71MK7Ir5t0PhYE"
-elif [ "$parse_branch" == "aosp/android-3.18" || "LA.UM.8.6.r1" ]; then
+elif [ "$parse_branch" =~ aosp ]; then
 	export kernel_type=PuRe-CaF
 	export sticker="CAADBQADfAEAAn1Cwy6aGpFrL8EcbRYE"
 elif [ ! "$kernel_type" ]; then
@@ -26,14 +26,13 @@ export config_device2=riva_defconfig
 # Environment Vars
 export ARCH=arm64
 export TZ="Asia/Jakarta"
-export pack1=$(pwd)/zip1
-export pack2=$(pwd)/zip2
 export TELEGRAM_ID=$chat_id
+export pack=$(pwd)/anykernel-3
 export TELEGRAM_TOKEN=$token
 export product_name=GREENFORCE
 export device="Xiaomi Redmi 4A/5A"
-export KBUILD_BUILD_HOST=$CIRCLE_SHA1
-export KBUILD_BUILD_USER=github.com.fadlyas07
+export KBUILD_BUILD_HOST=$(whoami)
+export KBUILD_BUILD_USER=Mhmmdfadlyas
 export kernel_img=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 export commit_point=$(git log --pretty=format:'%h: %s (%an)' -1)
 export PATH=$(pwd)/clang/bin:$(pwd)/gcc/bin:$(pwd)/gcc32/bin:$PATH
@@ -44,8 +43,7 @@ git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linu
 git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r36 gcc32
 git clone --depth=1 https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-6284175 clang
 git clone --depth=1 https://github.com/fabianonline/telegram.sh telegram
-git clone --depth=1 https://github.com/fadlyas07/anykernel-3 zip1
-git clone --depth=1 https://github.com/fadlyas07/anykernel-3 zip2
+git clone --depth=1 https://github.com/fadlyas07/anykernel-3
 
 TELEGRAM=telegram/telegram
 tg_channelcast() {
@@ -86,7 +84,7 @@ make O=out ARCH=arm64 "$config_device2"
 tg_makeclang
 }
 
-# Time to compile Device 1
+# Compile device 1
 date1=$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')
 tg_makedevice1
 mv *.log $TEMP
@@ -96,15 +94,14 @@ if [[ ! -f "$kernel_img" ]]; then
 	exit 1
 fi
 curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="784548477"
-mv $kernel_img $pack1/zImage
-cd $pack1
-zip -r9q $product_name-$codename_device1-$kernel_type-$date1.zip * -x .git README.md LICENCE
+mv $kernel_img $pack/zImage
+cd $pack && zip -r9q $product_name-$codename_device1-$kernel_type-$date1.zip * -x .git README.md LICENCE
 cd ..
 
 # clean out & log before compile again
-rm -rf out/ $TEMP/*.log
+rm -rf out/ $TEMP/*.log $pack/zImage
 
-# Time to compile Device 2
+# Compile device 2
 date2=$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')
 tg_makedevice2
 mv *.log $TEMP
@@ -114,9 +111,8 @@ if [[ ! -f "$kernel_img" ]]; then
 	exit 1
 fi
 curl -F document=@$(echo $TEMP/*.log) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="784548477"
-mv $kernel_img $pack2/zImage
-cd $pack2
-zip -r9q $product_name-$codename_device2-$kernel_type-$date2.zip * -x .git README.md LICENCE
+mv $kernel_img $pack/zImage
+cd $pack && zip -r9q $product_name-$codename_device2-$kernel_type-$date2.zip * -x .git README.md LICENCE $(echo *.zip)
 cd ..
 
 toolchain_ver=$(cat $(pwd)/out/include/generated/compile.h | grep LINUX_COMPILER | cut -d '"' -f2)
@@ -127,5 +123,5 @@ tg_channelcast "<b>$product_name new build is available</b>!" \
 		"<b>Branch :</b> <code>$parse_branch</code>" \
 		"<b>Toolchain :</b> <code>$toolchain_ver</code>" \
 		"<b>Latest commit :</b> <code>$commit_point</code>"
-curl -F document=@$(echo $pack1/*.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
-curl -F document=@$(echo $pack2/*.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
+curl -F document=@$(echo $pack/$product_name-$codename_device1-$kernel_type-$date1.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
+curl -F document=@$(echo $pack/$product_name-$codename_device2-$kernel_type-$date2.zip) "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument" -F chat_id="$TELEGRAM_ID"
