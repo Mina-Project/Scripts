e234dd687fd3fd2de6b2837aab5130b84c90af7c
Mhmmdfdlyas! <fadlyardhians@gmail.com>
2020-03-29 07:13:24 +0700

    Update rolex_riva.sh

--- rolex_riva.sh
+++ rolex_riva.sh
@@ -41,7 +41,7 @@ mkdir $(pwd)/TEMP
 export TEMP=$(pwd)/TEMP
 git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r36 gcc
 git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r36 gcc32
-git clone --depth=1 https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-6284175 clang
+git clone --depth=1 https://github.com/crdroidmod/android_prebuilts_clang_host_linux-x86_clang-5900059 clang
 git clone --depth=1 https://github.com/fabianonline/telegram.sh telegram
 git clone --depth=1 https://github.com/fadlyas07/anykernel-3
 
