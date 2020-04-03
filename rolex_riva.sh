763396514da6a0db4bfcf028acc4225b4f6da1bb
Mhmmdfdlyas! <fadlyardhians@gmail.com>
2020-04-03 15:24:57 +0700

    Update rolex_riva.sh

--- rolex_riva.sh
+++ rolex_riva.sh
@@ -78,8 +78,6 @@ else
     tg_build() {
       make -j$(nproc) O=out \
 		      ARCH=arm64 \
-		      CC=clang \
-		      CLANG_TRIPLE=aarch64-linux-gnu- \
 		      CROSS_COMPILE=aarch64-linux-android- \
 		      CROSS_COMPILE_ARM32=arm-linux-androideabi-
     }
